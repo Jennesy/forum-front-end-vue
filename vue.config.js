5e50d586672ed19f6580f2a8ad57aa2e7f8e27e2
Jennesy <jennesy.dai@gmail.com>
2022-08-25 18:24:40 +0800

    feat: change to history mode

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+	transpileDependencies: true,
+	publicPath:
+		process.env.NODE_ENV === 'production' ? '/forum-front-end-vue/' : '/',
+	chainWebpack: (config) => {
+		config.plugin('html').tap((args) => {
+			args[0].title = '餐廳評論網'
+			return args
+		})
+	},
 })
